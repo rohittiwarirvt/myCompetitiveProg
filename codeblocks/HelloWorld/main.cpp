33d321793013fee0c6a219aac299a9c77b7ca50a
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-02-09 08:31:16 +0530

    Hello universe program

--- codeblocks/HelloWorld/main.cpp
+++ codeblocks/HelloWorld/main.cpp
@@ -1,7 +1,8 @@
+// This is my First Hello world CPP program
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    std::cout << "Hello Universe" << std::endl;
     return 0;
 }
