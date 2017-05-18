f238961216843feb71818a1d4b3264c85682c817
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-05-18 11:07:02 +0530

    Aded the problems which i have solved today

--- hackerrank/basic_implementation/addition_aint_simple.c
+++ hackerrank/basic_implementation/addition_aint_simple.c
@@ -1,4 +1,40 @@
+/*
+Jack is awesome. His friends call him little Einstein. To test him, his friends gave him a string. They told him to add the string with its reverse string and follow these rules:
+
+    Every ith character of string will be added to every ith character of reverse string.
+    Both string will contain only lower case alphabets(a-z).
+    Eg:- a+a=b,a+c=d,z+a=a (Refer to sample test cases for more details)
+
+Input:
+
+First line contains a value N denoting number of test cases. Next N lines contains string str.
+
+Output:
+
+For every string str output the string that Jack's friends wants from him.
+
+Constraints
+
+1 <= N <= 10
+
+1 <= str <= 10^5
+ */
 #include<stdio.h>
+#include<string.h>
 int main() {
-  return0;
+  int t,  i, sum, string_length;
+  char input[100001];
+  scanf("%d", &t);
+  while(t--) {
+    scanf("%s", input);
+    string_length = strlen(input);
+    for(i=0;i< string_length;i++ ) {
+      sum = input[i] + input[string_length -1-i]-192;
+      if (sum > 26)
+        sum -=26;
+      printf("%c", sum + 96);
+    }
+    printf("\n");
+  }
+  return 0;
 }
--- hackerrank/basic_implementation/digit_problem.c
+++ hackerrank/basic_implementation/digit_problem.c
@@ -1,4 +1,130 @@
+/*
+This time your task is simple.
+
+Given two integers X
+and K, find the largest number that can be formed by changing digits at atmost K places in the number X
+
+.
+
+Input:
+
+First line of the input contains two integers X
+and K
+
+separated by a single space.
+
+Output:
+
+Print the largest number formed in a single line.
+
+Constraints:
+
+    1≤X≤1018
+
+0≤K≤9
+
+4483 2
+9983
+ */
 #include<stdio.h>
+#include<string.h>
 int main() {
-  return0;
+  int K,i , str_length;
+  char string[19];
+  scanf("%s", string);
+  scanf("%d", &K);
+  str_length = strlen(string);
+  for (i=0;i<str_length && K >0;i++) {
+    if (string[i] =='9'){
+      continue;
+    }
+    else {
+      string[i]='9';
+      K--;
+    }
+  }
+
+  printf("%s\n", string);
+
+  return 0;
 }
+
+/*
+Editorials solutions
+
+
+    #include<bits/stdc++.h>
+    using namespace std;
+    #define ff first
+    #define ss second
+    #define pb push_back
+    #define mp make_pair
+    #define all(x) x.begin(),x.end()
+    #define sz(x) ((int)x.size())
+    #define eps 1e-9
+    const int MOD = 1e9+7;
+    typedef long long ll;
+    typedef pair<int,int> pii;
+    ll POWER[65];
+    ll power(ll a, ll b) {ll ret=1;while(b) {if(b&1) ret*=a;a*=a;if(ret>=MOD) ret%=MOD;if(a>=MOD) a%=MOD;b>>=1;}return ret;}
+    ll inv(ll x) {return power(x,MOD-2);}
+    void precompute() {
+      POWER[0]=1;
+      for(int i=1;i<63;i++) POWER[i]=POWER[i-1]<<1LL;
+    }
+    int main() {
+      // freopen("T ASK.in","r",stdin);freopen("TASK.out","w",stdout);
+      precompute();
+      ll n,k;
+      cin>>n>>k;
+      assert(n>=1LL and n<=1000000000000000000LL);
+      assert(k>=0 and k<=9);
+      vector<int> v;
+      while(n) {
+        v.pb(n%10);
+        n/=10;
+      }
+      reverse(all(v));
+      for(int i=0;i<sz(v) and k--;i++) {
+        if(v[i]==9) {
+          k++;
+          continue;
+        }
+        else v[i]=9;
+      }
+      for(auto it:v) cout<<it;
+      cout<<endl;
+      return 0;
+    }
+
+
+Tester solutions
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+        string s;
+        int k;
+
+        cin >> s >> k;
+
+        assert(k >= 0 && k <= 9);
+        assert(s.size() >= 1 && s.size() <= 19);
+
+        if ( s.size() == 19 ) assert(s == "1000000000000000000");
+
+        for ( int i = 0; i < s.size(); i++ ) assert(s[i] >= '0' && s[i] <= '9');
+
+        for ( int i = 0; i < s.size() && k > 0; i++ ) {
+            if ( s[i] == '9' ) continue;
+            s[i] = '9', k--;
+        }
+
+        cout << s << endl;
+
+        return 0;
+    }
+
+
+
+ */
