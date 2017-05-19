f26028b2f0856dc38612f61197895c59073b3a72
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-05-19 21:29:07 +0530

    Added the learnth today

--- hackerrank/basic_implementation/presidential_problem.c
+++ hackerrank/basic_implementation/presidential_problem.c
@@ -1,4 +1,24 @@
 #include<stdio.h>
 int main() {
-  return0;
+  int t, N, Q,i,L,R,j;
+  char input[1000];
+  scanf("%d",&t);
+  while(t--) {
+    scanf("%d", &N);
+    scanf("%d", &Q);
+    scanf("%s", input);
+
+    for(i=1;i<=Q;i++){
+      scanf("%d", &L);
+      scanf("%d",&R);
+      for (j=L-1;j<R;j++){
+        input[j%N] =  (input[j] -'a' +26)%26 +'a' -1;
+      }
+      printf("%s\n", input);
+
+    }
+
+    printf("%s\n", input);
+  }
+  return 0;
 }
--- hackerrank/basic_implementation/the_great_kian.c
+++ hackerrank/basic_implementation/the_great_kian.c
@@ -1,4 +1,19 @@
 #include<stdio.h>
 int main() {
-  return0;
+  int n,i,j,s1, s2,s3;
+  unsigned long input[1000];
+  scanf("%d",&n);
+  for(i=0;i<n;i++) {
+    scanf("%ld", &input[i]);
+  }
+  s1=s2=s3=0;
+  for (i=0;i<n;i++) {
+    s1+=input[i*3+1 -1];
+    s2+=input[3*i-1-1];
+    s3+=input[3*i+3-1];
+  }
+  printf("%d %d %d\n",s1,s2,s3);
+  return 0;
 }
+//5
+// 1 2 3 4 5
