451d5c046beba16b8eb8b7c5b8eda2464e1fe523
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-05-20 13:36:01 +0530

    Presidential problem

--- hackerrank/basic_implementation/presidential_problem.c
+++ hackerrank/basic_implementation/presidential_problem.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main() {
-  int t, N, Q,i,L,R,j;
+  int t, N, Q,i,L,R,j,k, val;
   char input[1000];
   scanf("%d",&t);
   while(t--) {
@@ -11,13 +11,12 @@ int main() {
     for(i=1;i<=Q;i++){
       scanf("%d", &L);
       scanf("%d",&R);
-      for (j=L-1;j<R;j++){
-        input[j%N] =  (input[j] -'a' +26)%26 +'a' -1;
+      for (j=L-1,k=0;k<R;k++,j++){
+        val = (input[j%(N+1)] -'a' +26)%26 +'a';
+        input[j%(N+1)] = val == 97 ? 'z' : val -1;
       }
-      printf("%s\n", input);
 
     }
-
     printf("%s\n", input);
   }
   return 0;
