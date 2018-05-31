bc975a861194e9d889ac89c54b21e7f647aa8189
Rohit Tiwari <rohittiwarirvt@gmail.com>
2018-05-31 19:52:33 +0530

    Added code for finding index

---
+++ interview/find-index-0-replaced-1-get-longest-continuous-sequence-1s-binary-array.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+
+int maxOnesIndex(int arr[], int n) {
+
+  int max_index= -1;
+  int max_count =0;
+  int curr_index = 0;
+  int prev_index = -1;
+  int prev_prev_index = -1;
+
+  for(int i=0;i <  n; i++) {
+    if (arr[i] == 0) {
+      curr_index= i;
+      if (curr_index - prev_prev_index > max_count) {
+        max_count = curr_index- prev_prev_index;
+        max_index = prev_index;
+      }
+
+      prev_prev_index = prev_index;
+      prev_index = curr_index;
+    }
+  }
+
+  if ( n - prev_prev_index > max_count) {
+    max_index = prev_index;
+
+  }
+
+  return max_index;
+}
+
+int main() {
+  int  arr[] =  {1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1};
+  int n = 13;
+
+  int result;
+
+  result = maxOnesIndex(arr, n);
+  printf("%d\n", result);
+  return 0;
+}
